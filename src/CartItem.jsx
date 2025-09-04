086672312f8e857fc842e8bdc3518ab359824486
ellandis <elamarlandis@gmail.com>
2025-09-04 12:02:37 -0400

    need to fix something in cartslice or productlist

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach( item => {
+      total += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert("Checkout feature coming soon!");
+  }
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = item.quantity * parseFloat(item.cost.substring(1));
+    return totalCost.toFixed(2);
   };
 
   return (
